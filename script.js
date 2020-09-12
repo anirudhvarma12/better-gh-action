34963e6c2d5dd5e671994547d60ce7708fc785c1
Anirudh Varma <varma93@hotmail.com>
2020-09-13 01:58:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 console.log("GITHUB", process.env);
-fs.readFile(`${process.env.RUNNER_WORKSPACE}/defaultlist.json`, function (err, file) {
+fs.readFile(`${process.env.GITHUB_WORKSPACE}/defaultlist.json`, function (err, file) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
