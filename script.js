82a2ca0324238a9490bbd1e5b08d017e55a2eb6d
Anirudh Varma <anirudh@spotdraft.com>
2020-09-13 02:02:15 +0530

    Log the right stuff

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
-console.log("GITHUB", process.env);
-fs.readFile(`${process.env.RUNNER_WORKSPACE}/defaultlist.json`, function (err, file) {
+fs.readFile(`${process.env.GITHUB_WORKSPACE}/defaultlist.json`, function (err, file) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
-  console.log("FILE ", file.length);
-  //   const list = JSON.parse(file.join());
+  const database = JSON.parse(file.join());
+  const issueTitle = gh_context.event.issue;
+  const issueDescription = gh_context.event.body;
+  console.log("Number of links", database.length);
+  console.log("Issue Title", issueTitle, "Description", issueDescription);
 });
