3de88cb892579fd86808c0659958cdef396e9e5b
Anirudh Varma <anirudh@spotdraft.com>
2020-09-13 02:08:27 +0530

    Fix file read

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ fs.readFile(`${process.env.GITHUB_WORKSPACE}/defaultlist.json`, function (err, f
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
-  const database = JSON.parse(file.join());
-  const issueTitle = gh_context.event.issue;
-  const issueDescription = gh_context.event.body;
+  const database = JSON.parse(file.toString());
+  const issueTitle = process.env.gh_context.event.issue;
+  const issueDescription = process.env.gh_context.event.body;
   console.log("Number of links", database.length);
   console.log("Issue Title", issueTitle, "Description", issueDescription);
 });
