b54cea636502d60241a82853f8bca3743c680832
Anirudh Varma <anirudh@spotdraft.com>
2020-09-13 01:47:28 +0530

    Read Defaultlist

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 
 console.log("GITHUB", process.env);
-fs.readdir(process.env.RUNNER_WORKSPACE, function (err, files) {
-  //handling error
+fs.readFile(`${process.env.RUNNER_WORKSPACE}/defaultlist.json`, function (err, file) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
-  //listing all files using forEach
-  files.forEach(function (file) {
-    // Do whatever you want to do with the file
-    console.log("File,", file);
-  });
+  console.log("FILE ", file.length);
+  //   const list = JSON.parse(file.join());
 });
