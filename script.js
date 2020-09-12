f886a56551ef5e0b7a43d7cba0b8fd3fc900eaef
Anirudh Varma <anirudh@spotdraft.com>
2020-09-13 02:39:37 +0530

    add pull request step

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const fs = require("fs");
 
+function parseURLFromIssueTitle(title) {
+  return title.replace("Suggestion:", "").trim();
+}
+
+function findExistingMatchForUrl(url, list) {
+  return list.findIndex((listItem) => {
+    const match = url.match(new RegExp(listItem.urlPattern));
+    return match && match.alternatives;
+  });
+}
+
 fs.readFile(`${process.env.GITHUB_WORKSPACE}/defaultlist.json`, function (err, file) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
   const database = JSON.parse(file.toString());
   const gh_context = JSON.parse(process.env.gh_context);
-  const issueTitle = gh_context.event.issue;
-  const issueDescription = gh_context.event.body;
-  console.log("Number of links", database.length);
-  console.log("Issue Title", issueTitle, "Description", issueDescription);
+  const issueTitle = gh_context.event.issue.title;
+  const [suggestedUrl, name, ...descriptions] = gh_context.event.issue.body.split("\n");
+  const url = parseURLFromIssueTitle(issueTitle);
+  const existingIndex = findExistingMatchForUrl(url, database);
+  console.log("Found Existing", existingIndex);
+  if (existingIndex === -1) {
+    const item = {
+      urlPattern: url,
+      alternatives: [
+        {
+          url: suggestedUrl,
+          name: name,
+          desc: descriptions.join(),
+        },
+      ],
+    };
+    database.push(item);
+  } else {
+    database[existingIndex].alternatives.push({
+      url: url,
+      name: url,
+      desc: issueDescription,
+    });
+  }
+  fs.writeFileSync(`${process.env.GITHUB_WORKSPACE}/defaultlist.json`, JSON.stringify(database));
+  console.log("FILE", JSON.stringify(database));
 });
