4b9745203bcd42a65337fb70e4e589c5a96f2e6b
Anirudh Varma <anirudh@spotdraft.com>
2020-09-13 01:38:59 +0530

    List files

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// const gh = process.env
+const fs = require("fs");
+
 console.log("GITHUB", process.env);
+fs.readdir(process.env.RUNNER_WORKSPACE, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    // Do whatever you want to do with the file
+    console.log("File,", file);
+  });
+});
